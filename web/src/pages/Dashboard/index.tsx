3544a5a84e42137cfc1d4d8a38c2122ba83880a7
Marcos L <marcosrlj@outlook.com>
2020-06-02 05:02:51 -0300

    Conectando a API
    Lidando com erros

--- web/src/pages/Dashboard/index.tsx
+++ web/src/pages/Dashboard/index.tsx
@@ -1,47 +1,81 @@
-import React from 'react';
+import React, { useState, FormEvent } from 'react';
 import { FiChevronRight } from 'react-icons/fi';
 
+import api from '../../services/api';
 import logo from '../../assets/logo.svg';
 
-import { Title, Form, Repository } from './styles';
+import { Title, Form, Repository, ErrorMessage } from './styles';
+
+interface Repository {
+	full_name: string;
+	owner: {
+		login: string;
+		avatar_url: string;
+	};
+	description: string;
+}
 
 const Dashboard: React.FC = () => {
+	const [newRepo, setNewRepo] = useState('');
+	const [inputError, setInputError] = useState('');
+	const [repositories, setRepositories] = useState<Repository[]>([]);
+
+	async function handleAddRepository(
+		event: FormEvent<HTMLFormElement>,
+	): Promise<void> {
+		event.preventDefault();
+
+		if (!newRepo) {
+			setInputError('Digite o autor/nome do repositório');
+			return;
+		}
+
+		try {
+			const response = await api.get<Repository>(`repos/${newRepo}`);
+
+			console.log('response :', response.data);
+
+			const repository = response.data;
+
+			setRepositories([...repositories, repository]);
+			setNewRepo('');
+			setInputError('');
+		} catch (error) {
+			setInputError('Repositório não foi encontrado');
+		}
+	}
+
 	return (
 		<>
 			<img src={logo} alt="GitHub Explorer" />
 			<Title>Explore repositórios no GitHub.</Title>
-			<Form>
-				<input placeholder="Digite o nome do repositório" />
+			<Form hasError={Boolean(inputError)} onSubmit={handleAddRepository}>
+				<input
+					placeholder="Digite o nome do repositório"
+					value={newRepo}
+					onChange={e => setNewRepo(e.target.value)}
+				/>
 				<button type="submit">Pesquisar</button>
 			</Form>
 
+			{inputError && <ErrorMessage>{inputError}</ErrorMessage>}
+
 			<Repository>
-				<a href="">
-					<img
-						src="https://avatars3.githubusercontent.com/u/34945925?s=460&u=6edadeac17d1837992e1527f2d6d4521b31ba830&v=4"
-						alt="Marcos"
-					/>
+				{repositories.map(repository => (
+					<a key={repository.full_name} href="">
+						<img
+							src={repository.owner.avatar_url}
+							alt={repository.owner.login}
+						/>
 
-					<div>
-						<strong>repositorio</strong>
-						<p>descriçao</p>
-					</div>
+						<div>
+							<strong>{repository.full_name}</strong>
+							<p>{repository.description}</p>
+						</div>
 
-					<FiChevronRight size={20} />
-				</a>
-				<a href="">
-					<img
-						src="https://avatars3.githubusercontent.com/u/34945925?s=460&u=6edadeac17d1837992e1527f2d6d4521b31ba830&v=4"
-						alt="Marcos"
-					/>
-
-					<div>
-						<strong>repositorio</strong>
-						<p>descriçao</p>
-					</div>
-
-					<FiChevronRight size={20} />
-				</a>
+						<FiChevronRight size={20} />
+					</a>
+				))}
 			</Repository>
 		</>
 	);
